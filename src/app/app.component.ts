837e08f46ef697e1405ca4e6a6e979a12c8a1841
Vladimir Pavković <vladimirpavk@gmail.com>
2018-03-20 12:55:47 +0100

    Exercise completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements: {type:string, name:string, content:string}[] = [];
+  /*serverElements: {type:string, name:string, content:string}[] = [];
 
   added(event: {type:string, name:string, content:string}):void
   {
     this.serverElements.push(event);
     //console.log(this.serverElements);
+  }*/
+
+  public oddNumbers: number[] = [];
+  public evenNumbers: number[] = [];
+  
+  public tickTack(counter:number):void{
+    if( this.isEven(counter) ){
+      this.evenNumbers.push(counter);
+      console.log(this.evenNumbers);
+    }
+    else{
+      this.oddNumbers.push(counter);
+      console.log(this.oddNumbers);
+    }
   }
+
+  public isEven(value: number):boolean{
+    let division=value/2;
+    if(Math.trunc(division)*2==value) return true;
+    return false;
+  }
+  
 }
---
+++ src/app/game-control/game-control.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-game-control',
+  templateUrl: './game-control.component.html',
+  styleUrls: ['./game-control.component.css']
+})
+export class GameControlComponent implements OnInit {
+
+  private ref:number=0
+  private tickCount: number=0;
+  @Output() tick: EventEmitter<number> = new EventEmitter<number>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onStartButtonClicked(){
+    if(this.ref===0)
+    {
+      this.ref=setInterval(
+        ()=>{
+          this.tick.emit(this.tickCount++);
+      }, 1000);
+    }
+  }
+
+  onStopButtonClicked(){
+    clearInterval(this.ref);
+    this.ref=0;
+  }
+
+}
